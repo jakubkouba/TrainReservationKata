aa7ed36bdf0bdd679b25b05caf818528ee75c030
Jakub Adler <adler.jakub@gmail.com>
2018-08-21 19:59:17 +0100

    Reserve one seat in the train

--- reservation_app/app/spec/ticket_office_spec.rb
+++ reservation_app/app/spec/ticket_office_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../ticket_office.rb'
+require_relative '../train.rb'
 
 describe TicketOffice do
 
@@ -8,6 +9,20 @@ describe TicketOffice do
 
     it 'returns request' do
       expect(ticket_office.make_reservation(request)).to eq 'request string'
+    end
+  end
+
+  describe 'train is empty' do
+    describe 'and passenger reserves one ticket' do
+      let(:train) { Train.new }
+
+      before { allow(train).to receive(:empty?).and_return(true) }
+
+      it 'reserves one seat' do
+        reservation = train.reserve_seats(1)
+
+        expect(reservation).to eq(['ticket123', '1A'])
+      end
     end
   end
 end
---
+++ reservation_app/app/train.rb
@@ -0,0 +1,5 @@
+class Train
+  def reserve_seats(number_of_seats)
+    ['ticket123', '1A']
+  end
+end
