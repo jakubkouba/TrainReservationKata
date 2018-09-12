6fea99ab9cf445c37617edffdc6c31f75c052799
Jakub Adler <adler.jakub@gmail.com>
2018-09-12 13:24:32 +0100

    Book any number of seats in empty train

--- reservation_app/app/features/step_definitions/book_tickets.rb
+++ reservation_app/app/features/step_definitions/book_tickets.rb
@@ -2,12 +2,13 @@ Given(/^Empty train$/) do
   # mock empty train
 end
 When(/^Customer reserves seat 1A in train Express_2000 with booking reference booking_123$/) do
-  @payload = { train_id: 'Express_2000', seats: ['1A'], booking_reference: 'booking_123' }
+  @payload = { train_id: 'Express_2000', seats: ['1A'] }
 end
 
 Then(/^System reserves one seat$/) do
   post 'http://localhost:8083/reserve', @payload
-  expect(json_response).to eq({ 'train_id' => 'Express_2000', 'seats' => ['1A'], 'booking_reference' => 'booking_123' })
+  expect(json_response).to include(train_id: 'Express_2000', seats: ['1A'])
+  expect(json_response).to include(:booking_reference)
 end
 
 When(/^When Customer reserves seat 1A and 2A in train Express_2000 with booking reference booking_456$/) do
@@ -16,6 +17,6 @@ end
 
 Then(/^System reserves two seats$/) do
   post 'http://localhost:8083/reserve', @payload
-  expect(json_response).to eq({ 'train_id' => 'express_2000', 'seats' => ['1A', '2A'], 'booking_reference' => 'booking_456' })
-
+  expect(json_response).to include( train_id: 'Express_2000', seats: ['1A', '2A'])
+  expect(json_response).to include(:booking_reference)
 end
--- reservation_app/app/ticket_office_on_sinatra.rb
+++ reservation_app/app/ticket_office_on_sinatra.rb
@@ -3,7 +3,9 @@
 # before you'll be able to run this you'll need 'gem install sinatra'
 
 require 'sinatra'
+require 'json'
 require 'byebug'
+require 'securerandom'
 
 require_relative './lib/ticket_office'
 
@@ -12,12 +14,27 @@ get '/' do
 end
 
 post '/reserve' do
-  # office = TicketOffice.new()
-  # office.make_reservation(request.body)
+  # office = TicketOffice.new
+  # booked_seats = office.make_reservation(train_id: params[:train_id], seats: params[:seats])
 
-  { train_id: 'Express_2000', seats: ['1A'], booking_reference: 'booking_123'}.to_json
+  seats = JSON.parse(File.read('./data/trains.json')).with_indifferent_access
+  booked_seats = []
+  seats[params[:train_id].downcase][:seats].each do |seat_number, seat_properties|
+    if params[:seats].include? seat_number.to_s
+      seat_properties[:booking_reference] = params[:booking_reference]
+      booked_seats << seat_number
+    end
+  end
+
+  { train_id: params[:train_id], seats: booked_seats, booking_reference: BookingReferenceNumber.generate }.to_json
 end
 
 configure do
   set :port, '8083'
+end
+
+class BookingReferenceNumber
+  def self.generate
+    SecureRandom.uuid
+  end
 end
