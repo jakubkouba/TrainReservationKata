fc6fc0bf808aee97a2c07cdb0cc3db685dd62f80
Jakub Adler <adler.jakub@gmail.com>
2018-09-20 13:46:53 +0100

    Reserve two seats in train

--- reservation_app/app/lib/ticket_office.rb
+++ reservation_app/app/lib/ticket_office.rb
@@ -1,8 +1,28 @@
 class TicketOffice
+
+  attr_reader :train_data_service, :booking_reference_service
+
+  def initialize(train_data_service, booking_reference_service)
+    @train_data_service = train_data_service
+    @booking_reference_service = booking_reference_service
+  end
+
   def make_reservation(train_id:, seats:)
-    ['1A']
+    train = train_data_service.find_train(train_id)
+    reserved_seats = train.reserve_seats(seats, booking_reference_service.reservation_number)
+    Reservation.new(reserved_seats, train_id)
   end
 end
 
+class Reservation
+
+  attr_reader :seats, :train_id
+
+  def initialize(seats, train_id)
+    @seats = seats
+    @train_id = train_id
+  end
+
+end
 
 
--- reservation_app/app/spec/ticket_office_spec.rb
+++ reservation_app/app/spec/ticket_office_spec.rb
@@ -4,13 +4,48 @@ describe TicketOffice do
 
   describe 'Reserve one seat in empty train' do
 
-    it 'reserves 1A seat' do
+    it 'reserves a seat' do
       train_id = 'Express_2000'
-      ticket_office = TicketOffice.new
       seats = 1
 
-      reserved_seats = ticket_office.make_reservation(train_id: train_id, seats: seats)
-      expect(reserved_seats).to eq(['1A'])
+      train_data_service = double('TrainDataService')
+      booking_reference_service = double('BookingReferenceNumber')
+      reservation_number = 15678
+      train = double('Train')
+
+      allow(train_data_service).to receive(:find_train).with(train_id).and_return(train)
+      allow(booking_reference_service).to receive(:reservation_number).and_return(reservation_number)
+      allow(train).to receive(:reserve_seats).with(seats, reservation_number).and_return(%w[1A])
+      allow(train_data_service).to receive(:reserve).with(%w[1A])
+
+      ticket_office = TicketOffice.new(train_data_service, booking_reference_service)
+
+      reservation = ticket_office.make_reservation(train_id: train_id, seats: seats)
+
+      expect(reservation).to have_attributes(seats: %w[1A], train_id: 'Express_2000')
+    end
+  end
+
+  describe 'Reserve two seats in empty train' do
+
+    it 'reserves 1A and 2A seats' do
+      train_id = 'Express_2000'
+      seats = 2
+
+      train_data_service = double('TrainDataService')
+      booking_reference_service = double('BookingReferenceNumber')
+      reservation_number = 15678
+      train = double('Train')
+
+      allow(train_data_service).to receive(:find_train).with(train_id).and_return(train)
+      allow(booking_reference_service).to receive(:reservation_number).and_return(reservation_number)
+      allow(train).to receive(:reserve_seats).with(seats, reservation_number).and_return(%w[1A 2A])
+
+      ticket_office = TicketOffice.new(train_data_service, booking_reference_service)
+
+      reservation = ticket_office.make_reservation(train_id: train_id, seats: seats)
+
+      expect(reservation).to have_attributes(seats: %w[1A 2A], train_id: 'Express_2000')
     end
   end
 end
