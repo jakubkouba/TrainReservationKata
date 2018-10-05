edd9643c24f262a58d0b73a7b7481a70c27828cb
Jakub Adler <adler.jakub@gmail.com>
2018-10-05 14:04:21 +0100

    Specs refactoring WIP

--- reservation_app/app/spec/ticket_office_spec.rb
+++ reservation_app/app/spec/ticket_office_spec.rb
@@ -3,16 +3,13 @@ require 'spec_helper'
 describe TicketOffice do
 
   describe 'Reserve one seat in train' do
-
-    it 'reserves seat 1A' do
-      train_id = 'express_2000'
-      seats = 1
-
-      train_data_service = double('TrainDataService')
-      booking_reference_service = double('BookingReferenceNumber')
-      reservation_number = '15678'
-
-      trains = {
+    let(:train_data_service) { double('TrainDataService') }
+    let(:booking_reference_service) { double('BookingReferenceNumber') }
+    let(:train_id) { 'express_2000' }
+    let(:seats) { 1 }
+    let(:reservation_number) { '15678' }
+    let(:trains) do
+      {
         'express_2000' => {
           'seats' => {
             '1A' => { 'coach' => 'A', 'seat_number' => '1', 'booking_reference' => "" },
@@ -23,30 +20,28 @@ describe TicketOffice do
           }
         }
       }
+    end
 
+    let(:ticket_office) { TicketOffice.new(train_data_service, booking_reference_service) }
+    subject(:reservation) { ticket_office.make_reservation(train_id: train_id, seats: seats) }
+
+    it 'reserves seat 1A' do
       allow(train_data_service).to receive(:trains).and_return(trains)
       allow(train_data_service).to receive(:reserve)
       allow(booking_reference_service).to receive(:reservation_number).and_return(reservation_number)
-
-      ticket_office = TicketOffice.new(train_data_service, booking_reference_service)
-
-      reservation = ticket_office.make_reservation(train_id: train_id, seats: seats)
 
       expect(reservation).to have_attributes(seats: %w[1A], train_id: 'express_2000')
     end
   end
 
   describe 'Reserve two seats in train' do
-
-    it 'reserves seats 1A and 2A' do
-      train_id = 'express_2000'
-      seats = 2
-
-      train_data_service = double('TrainDataService')
-      booking_reference_service = double('BookingReferenceNumber')
-      reservation_number = '15678'
-
-      trains = {
+    let(:train_id) { 'express_2000' }
+    let(:seats) { 2 }
+    let(:train_data_service) { double('TrainDataService') }
+    let(:booking_reference_service) { double('BookingReferenceNumber') }
+    let(:reservation_number) { '15678' }
+    let(:trains) do
+      {
         'express_2000' => {
           'seats' => {
             '1A' => { 'coach' => 'A', 'seat_number' => '1', 'booking_reference' => "" },
@@ -57,15 +52,16 @@ describe TicketOffice do
           }
         }
       }
+    end
 
+    let(:ticket_office) { TicketOffice.new(train_data_service, booking_reference_service) }
+    subject(:reservation) { ticket_office.make_reservation(train_id: train_id, seats: seats) }
+
+    it 'reserves seats 1A and 2A' do
       allow(train_data_service).to receive(:trains).and_return(trains)
       allow(train_data_service).to receive(:reserve)
       allow(booking_reference_service).to receive(:reservation_number).and_return(reservation_number)
-
-      ticket_office = TicketOffice.new(train_data_service, booking_reference_service)
-
-      reservation = ticket_office.make_reservation(train_id: train_id, seats: seats)
-
+      
       expect(reservation).to have_attributes(seats: %w[1A 2A], train_id: 'express_2000')
     end
   end
