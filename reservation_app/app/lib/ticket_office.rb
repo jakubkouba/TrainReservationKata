9fea3c9bcf6825ae731ee8d33ae8438be2c1da4a
Jakub Adler <adler.jakub@gmail.com>
2018-10-11 13:48:48 +0100

    Catlculate train ccupation in separate method

--- reservation_app/app/lib/ticket_office.rb
+++ reservation_app/app/lib/ticket_office.rb
@@ -10,6 +10,8 @@ class TicketOffice
     @booking_reference_service = booking_reference_service
   end
 
+  MAX_PCT_CAPACITY_ALLOWANCE = 70
+
   def make_reservation(train_id:, seats:)
     seat_list = train_data_service.train(train_id)
 
@@ -17,15 +19,18 @@ class TicketOffice
 
     seats_to_reserve = free_seats.keys.first(seats)
 
-    reserved_seats = seat_list.count - seats_to_reserve.count
-    filled = (reserved_seats / seat_list.count.to_f) * 100
-    raise OverbookedException if filled >= 70
+    raise OverbookedException if in_advance_reserved_seats(seat_list, seats_to_reserve) >= MAX_PCT_CAPACITY_ALLOWANCE
 
     train_data_service.reserve(train_id, seats_to_reserve, booking_reference_service.reservation_number)
     Reservation.new(train_id, seats_to_reserve)
   end
 
   private
+
+  def in_advance_reserved_seats(seat_list, seats_to_reserve)
+    reserved_seats = seat_list.count - seats_to_reserve.count
+    (reserved_seats / seat_list.count.to_f) * 100
+  end
 
   def free_seat?(seat_property)
     seat_property['booking_reference'].empty?
