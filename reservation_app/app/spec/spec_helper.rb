9873776ef144a1d424a978da14c9baea7fbf0504
Jakub Adler <adler.jakub@gmail.com>
2018-10-12 14:15:20 +0100

    Enable focus on specs

--- reservation_app/app/spec/spec_helper.rb
+++ reservation_app/app/spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'webmock/rspec'
 
 Dir["#{File.expand_path('../lib', File.dirname(File.absolute_path(__FILE__)))}/**/*.rb"].each { |file| require file }
+
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+end
